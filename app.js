84dfe2bddfd1d8aa6909ca30a74c82121a85395e
Subhransu Behera <152528962+Behera873@users.noreply.github.com>
2025-10-28 18:41:33 +0530

    Modify server response message for webhook implementation
    Updated response message to include webhook implementation.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello from Node.js server running on VS Code!');
+  res.end('Hello from Node.js server running on VS Code! - \n Now I am implement webhook for complete cicd');
 });
 
 server.listen(3000, '0.0.0.0', () => {
