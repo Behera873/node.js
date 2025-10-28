bede52bb991e1f516c50f57a71ede74379c02b44
Subhransu Behera <152528962+Behera873@users.noreply.github.com>
2025-10-28 15:56:07 +0530

    upload all files

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello from Node.js server running on VS Code!');
+});
+
+server.listen(3000, '0.0.0.0', () => {
+  console.log('✅ Server running on http://localhost:3000');
+});
